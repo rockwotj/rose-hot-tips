6ca20b80023f645452a5bfc47c74186a0ea5a524
rockwotj <rockwotj@rose-hulman.edu>
2014-10-22 15:51:38 -0400

    Fixed Render for Sam's Script

--- scripts/course_script.py
+++ scripts/course_script.py
@@ -17,7 +17,7 @@ class CourseFileParser:
 def run():
 	""" The function that will be called put the course data into the Datastore. """
 	logging.info("Started Parsing Course Information from CSV file.")
-	csvfile = main.jinja_env.get_template("templates/2014_course_catalog.csv").render()
+	csvfile = main.jinja_env.get_template("templates/2014_course_catalog.csv").render({})
 	parser = CourseFileParser()
 	# TODO: Parse the csvFile and put it's data into Course Entities from models.py
 	logging.info("Ended Parsing Course Information.")
